4b1e23c428b55392f71ec2439533851311aac620
Willkins kenyatta <128659684+Willkins3951@users.noreply.github.com>
2024-03-07 02:56:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// script.js
+const menuButton = document.getElementById('menu-toggle');
+
+/*const navbarLinks = document.querySelector('.navbar-Links');
+*/
+const navbarLinks = document.querySelector('.links');
+
+
+
+
+menuButton.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+/*
+const toggleicon = document.querySelector('.toggle-icon');
+
+toggleicon.addEventListener('click', () => {
+    toggleicon.classList.toggle('fa-sun');
+    document.body.classList.toggle('dark-mode');
+       
+});
+*/
